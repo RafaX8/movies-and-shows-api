3f945d5de651a4a19a5d220b5ef03b24f707f9ad
RafaX <rafita8md@gmail.com>
2022-11-08 12:11:32 +0100

    First Commit: beginning of project and files structure.

---
+++ app/src/main/java/com/rafaelmardom/movies_and_shows_api/presentation/MoviesFeedActivity.kt
@@ -0,0 +1,12 @@
+package com.rafaelmardom.movies_and_shows_api.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rafaelmardom.movies_and_shows_api.R
+
+class MoviesFeedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movies_feed)
+    }
+}
