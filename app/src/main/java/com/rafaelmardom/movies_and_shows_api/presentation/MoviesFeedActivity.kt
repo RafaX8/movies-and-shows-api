32989e136b6c6733d4215719597188ab2c073f6f
RafaX8 <rafita8md@gmail.com>
2022-11-11 17:59:55 +0100

    Commit to delete an unnecessary test.

--- app/src/main/java/com/rafaelmardom/movies_and_shows_api/presentation/MoviesFeedActivity.kt
+++ app/src/main/java/com/rafaelmardom/movies_and_shows_api/presentation/MoviesFeedActivity.kt
@@ -2,10 +2,7 @@ package com.rafaelmardom.movies_and_shows_api.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import com.rafaelmardom.movies_and_shows_api.R
-import com.rafaelmardom.movies_and_shows_api.data.ApiClient
-import kotlin.concurrent.thread
 
 class MoviesFeedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
